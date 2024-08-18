383d148198bb3e2ceabd3c2dbfe370f43f3cd1f7
Balsero300 <lukitasantarelli@gmail.com>
2024-08-18 16:05:19 -0300

    importacion de navigationStack

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const stack = createStackNavigator()
+
+function myStack() {
+  return (
+    <Stack.Navigation>
+      <Stack.screen/>
+    </Stack.Navigation>
+  )
+}
 export default function App() {
   return (
     <NavigationContainer>
